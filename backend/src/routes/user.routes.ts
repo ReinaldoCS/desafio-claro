7a3c8de5df693aee3e7cde30bee589b1519cca57
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-05-01 15:04:25 -0300

    Criando rota da cadastro

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,15 +1,37 @@
 import { Router, Request, Response } from 'express';
-
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();;
-
+const prisma = new PrismaClient();
 const helloWorldRouter = Router();
 
-helloWorldRouter.get('/create', async (req: Request, res: Response) => {
+helloWorldRouter.post('/create', async (req: Request, res: Response) => {
   try {
-    const db = await prisma.user.findMany();
-    return res.status(200).json({ message: "Hello World!!!", db });
+    const { email, username, password } = req.body;
+
+    if(!email || !username || !password) {
+      return res.status(400).json({message: "MISSING_VALUE"});
+    }
+
+    const findEmail = await prisma.user.findFirst({ where: { email }  });
+    const findUser = await prisma.user.findFirst({ where: { username } });
+
+    if (findEmail) {
+      return res.status(400).json({ message: "EMAIL_IN_USE" })
+    };
+
+    if (findUser) {
+      return res.status(400).json({ message: "USER_IN_USE" })
+    };
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password
+      }
+    });
+
+    return res.status(200).json({ message: "SUCESS", newUser });
   } catch (error) {
     return res.status(400).json(error);
   }
