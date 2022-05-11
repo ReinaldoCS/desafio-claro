2df89f837a7268e53f323dd0fb89a29109677115
reinaldocs <santosdoscorreiareinaldo@gmail.com>
2022-05-10 21:50:20 -0300

    Configurando servidor para o Frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(router);
 
